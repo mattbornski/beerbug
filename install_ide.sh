d5376fa3a2ced0815d12282e2462be4ea72b5180
Matt Born <mtborn@gmail.com>
2010-10-23 09:22:23 -0700

    Since the server-side will become a much more important part of this project, give it it's own directory to organize the files.

--- install_ide.sh
+++ install_ide.sh
@@ -4,18 +4,19 @@ ARDUINO="arduino-0021"
 VIRTUALWIRE="VirtualWire-1.5"
 DHCP="Arduino-DHCPv0.4"
 
-BASE=`readlink -f \`dirname $0\``
+USER_BASE=`readlink -f \`dirname $0\``
+FILES_BASE=$USER_BASE/embedded
 
-pushd $BASE
+pushd $FILES_BASE
 rm -rf bin
 mkdir bin
 cd bin
-tar xvf $BASE/toolkits/$ARDUINO.tgz
+tar xvf $FILES_BASE/toolkits/$ARDUINO.tgz
 cd $ARDUINO/libraries
-unzip $BASE/toolkits/$VIRTUALWIRE.zip
-unzip $BASE/toolkits/$DHCP.zip
-cd $BASE
+unzip $FILES_BASE/toolkits/$VIRTUALWIRE.zip
+unzip $FILES_BASE/toolkits/$DHCP.zip
 
+cd $USER_BASE
 rm run_ide.sh
 echo "#!/bin/bash" >> run_ide.sh
 echo "" >> run_ide.sh

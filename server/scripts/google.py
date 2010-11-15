a3ad384bc52dd1e94c8304e6f0df354e70be4701
Matt Born <mattborn@milton.(none)>
2010-11-14 17:11:27 -0800

    working up the separation of google-ward communication and beerbug-ward communication

--- server/scripts/google.py
+++ server/scripts/google.py
@@ -11,100 +11,107 @@ import sys
 import time
 import traceback
 
-def readSample(port):
-    # Not the most efficient way to communicate over the port,
-    # but I'm not exactly I/O limited here.
-    try:
-        temp = float(port.readline().split()[0])
-        return temp
-    except (ValueError, IndexError):
-        # Format was unexpected.  This can occur as communications can be
-        # disrupted.
-        pass
-
-def serve(username, password):
-    try:
-        while True:
-            port = None
-            # Detect which USB device the Arduino lives on.
-            for device in glob.glob('/dev/ttyUSB*'):
-                try:
-                    # TODO: Figure out how to determine if this is our
-                    # device or another device.
-                    port = serial.Serial(device)
-                except KeyboardInterrupt:
-                    # Do not handle, pass up to outside handler.
-                    raise KeyboardInterrupt
-                except:
-                    # Errors are to be expected while randomly probing
-                    # ports.
-                    pass
-            if port is not None:
-                client = gdata.spreadsheet.text_db.DatabaseClient(
-                  username = username, password = password)
-                db = client.GetDatabases(name = 'Beerbug Testing')[0]
-                table = db.GetTables()[0]
-                try:
-                    while True:
-                        now = datetime.datetime.now().strftime(
-                          '%m/%d/%Y %H:%M:%S')
-                        try:
-                            sample = readSample(port)
-                        except KeyboardInterrupt:
-                            # Do not handle, pass up to outside handler.
-                            raise KeyboardInterrupt
-                        except:
-                            # Throw the error and then attempt to continue.
-                            print >> sys.stderr, 'Problem reading sample.'
-                            traceback.print_exc(file=sys.stderr)
-                        if sample is not None:
-                            table.AddRecord(
-                              {'time':now, 'temperature':str(sample)},
-                            )
-                except KeyboardInterrupt:
-                    # Do not handle, pass up to outside handler.
-                    raise KeyboardInterrupt
-                except:
-                    # Throw the error and then attempt to continue.
-                    print >> sys.stderr, 'Problem uploading sample'
-                    traceback.print_exc(file=sys.stderr)
-            else:
-                # Notify user and attempt to continue later.
-                print >> sys.stderr, 'Unable to locate Arduino on a USB port.'
-                # Unlike some of the other errors, this one most likely
-                # requires human interaction to fix, so give them some time.
-                time.sleep(30)
-    except KeyboardInterrupt:
-        pass
-
-def serve_forever():
+class service(dict):
     username = None
     password = None
-    # Read the username and password from a config file.
-    # If not present, prompt the user for their credentials.
-    parser = ConfigParser.SafeConfigParser()
-    path = os.path.expanduser('~/.beerbug/beerbug.ini')
-    parser.read(path)
-    if parser is not None:
-        try:
-            try:
-                username = parser.get('google', 'username')
-            except ConfigParser.NoOptionError:
-                pass
-            try:
-                password = parser.get('google', 'password')
-            except ConfigParser.NoOptionError:
-                pass
-        except ConfigParser.NoSectionError:
-            pass
-    if username is None:
-        username = raw_input('Enter Google Docs username [' \
-          + getpass.getuser() + ']: ')
-        if username.strip() == '':
-            username = getpass.getuser()
-    if password is None:
-        password = getpass.getpass('Enter Google Docs password: ')
-    serve(username, password)
+    client = None
+    def __init__(self, username = None, password = None):
+        # Read the username and password from a config file.
+        # If not present, prompt the user for their credentials.
+        paths = [
+          os.path.expanduser('~/.beerbug/beerbug.ini'),
+          os.path.join(os.path.dirname(__file__), 'beerbug.ini'),
+          os.path.abspath('beerbug.ini'),
+        ]
+        for path in paths:
+            parser = ConfigParser.SafeConfigParser()
+            parser.read(path)
+            if parser is not None:
+                try:
+                    try:
+                        username = parser.get('google', 'username')
+                    except ConfigParser.NoOptionError:
+                        pass
+                    try:
+                        password = parser.get('google', 'password')
+                    except ConfigParser.NoOptionError:
+                        pass
+                except ConfigParser.NoSectionError:
+                    pass
+            if username is None:
+                username = raw_input('Enter Google Docs username [' \
+                  + getpass.getuser() + ']: ')
+                if username.strip() == '':
+                    username = getpass.getuser()
+            if password is None:
+                password = getpass.getpass('Enter Google Docs password: ')
+        if username is not None:
+            self.username = username
+        if password is not None:
+            self.password = password
+        # Connect
+        self.client = self.connect()
+        # Populate
+        self._index()
+    def connect(self):
+        # Read username and password from config files if not set.
+        
+        # Connect to Google
+        return gdata.spreadsheet.text_db.DatabaseClient(
+          username = self.username, password = self.password)
+    def _index(self):
+        for db in self.client.GetDatabases(name = ''):
+            self[db.entry.title.text] = spreadsheet(db)
+
+class spreadsheet(dict):
+    def __init__(self, db):
+        tables = db.GetTables()
+        for table in tables:
+            self[table.name] = page(table)
+
+class page(dict):
+    def __init__(self, table):
+        print table
+        print dir(table)
+        print table.fields
+        print table.entry
+        print table.LookupFields()
+        print table.GetRecords()
+#            try:
+#                while True:
+#                    now = datetime.datetime.now().strftime(
+#                      '%m/%d/%Y %H:%M:%S')
+#                    try:
+#                        sample = readSample(port)
+#                    except KeyboardInterrupt:
+#                        # Do not handle, pass up to outside handler.
+#                        raise KeyboardInterrupt
+#                    except:
+#                        # Throw the error and then attempt to continue.
+#                        print >> sys.stderr, 'Problem reading sample.'
+#                        traceback.print_exc(file=sys.stderr)
+#                    if sample is not None:
+#                        table.AddRecord(
+#                          {'time':now, 'temperature':str(sample)},
+#                        )
+#            except KeyboardInterrupt:
+#                # Do not handle, pass up to outside handler.
+#                raise KeyboardInterrupt
+#            except:
+#                # Throw the error and then attempt to continue.
+#                print >> sys.stderr, 'Problem uploading sample'
+#                traceback.print_exc(file=sys.stderr)
+#        else:
+#            # Notify user and attempt to continue later.
+#            print >> sys.stderr, 'Unable to locate Arduino on a USB port.'
+#            # Unlike some of the other errors, this one most likely
+#            # requires human interaction to fix, so give them some time.
+#            time.sleep(30)
+#    except KeyboardInterrupt:
+#        pass
+
+def serve_forever():
+    service()
 
 if __name__ == '__main__':
     serve_forever()
--- server/scripts/http.py
+++ server/scripts/http.py
@@ -22,60 +22,15 @@ def readSample(port):
         # disrupted.
         pass
 
-def serve(username, password):
-    try:
-        while True:
-            port = None
-            # Detect which USB device the Arduino lives on.
-            for device in glob.glob('/dev/ttyUSB*'):
-                try:
-                    # TODO: Figure out how to determine if this is our
-                    # device or another device.
-                    port = serial.Serial(device)
-                except KeyboardInterrupt:
-                    # Do not handle, pass up to outside handler.
-                    raise KeyboardInterrupt
-                except:
-                    # Errors are to be expected while randomly probing
-                    # ports.
-                    pass
-            if port is not None:
-                client = gdata.spreadsheet.text_db.DatabaseClient(
-                  username = username, password = password)
-                db = client.GetDatabases(name = 'Beerbug Testing')[0]
-                table = db.GetTables()[0]
-                try:
-                    while True:
-                        now = datetime.datetime.now().strftime(
-                          '%m/%d/%Y %H:%M:%S')
-                        try:
-                            sample = readSample(port)
-                        except KeyboardInterrupt:
-                            # Do not handle, pass up to outside handler.
-                            raise KeyboardInterrupt
-                        except:
-                            # Throw the error and then attempt to continue.
-                            print >> sys.stderr, 'Problem reading sample.'
-                            traceback.print_exc(file=sys.stderr)
-                        if sample is not None:
-                            table.AddRecord(
-                              {'time':now, 'temperature':str(sample)},
-                            )
-                except KeyboardInterrupt:
-                    # Do not handle, pass up to outside handler.
-                    raise KeyboardInterrupt
-                except:
-                    # Throw the error and then attempt to continue.
-                    print >> sys.stderr, 'Problem uploading sample'
-                    traceback.print_exc(file=sys.stderr)
-            else:
-                # Notify user and attempt to continue later.
-                print >> sys.stderr, 'Unable to locate Arduino on a USB port.'
-                # Unlike some of the other errors, this one most likely
-                # requires human interaction to fix, so give them some time.
-                time.sleep(30)
-    except KeyboardInterrupt:
-        pass
+class service:
+    schema = {'Beerbug Testing':['time', 'temperature']}
+    now = datetime.datetime.now().strftime(
+      '%m/%d/%Y %H:%M:%S')
+    sample = readSample(port)
+    if sample is not None:
+        table.AddRecord(
+          {'time':now, 'temperature':str(sample)},
+        )
 
 def serve_forever():
     username = None
